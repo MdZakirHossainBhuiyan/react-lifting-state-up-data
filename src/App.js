9d4f2af94f057a7edb595741963a0257f518f776
unknown <zakirhossaintuhin@gmail.com>
2022-08-14 05:59:21 +0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Counter from './Components/Counter';
+import States from './Components/States';
+
+const initialState = [
+  {
+    id: 1,
+    count: 0
+  },
+  {
+    id: 2,
+    count: 0
+  }
+]
 
 function App() {
+  const [state, setState] = useState(initialState)
+
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter.count, 0);
+  }
+
+  const increment = (id) => {
+    const updateCounter = state.map(item => {
+      if(item.id===id){
+        return {
+          ...item,
+          count: item.count + 1
+        }
+      }
+      else return {...item}
+    })
+
+    setState(updateCounter);
+  }
+
+  const decrement = (id) => {
+    const updateCounter = state.map(item => {
+      if(item.id===id){
+        return {
+          ...item,
+          count: item.count - 1
+        }
+      }
+      else return {...item}
+    })
+
+    setState(updateCounter);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+            <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+                Simple Counter Application
+            </h1>
+
+            <div class="max-w-md mx-auto mt-10 space-y-5">
+              {
+                state.map((count) => <Counter key={count.id} count={count.count} id={count.id} increment={increment} decrement={decrement} />)
+              }
+
+              <States count={totalCount()} />
+            </div>
+        </div>
   );
 }
 
